96290e1489c304ad95a7bfe6d685ee6d913877b7
Himanshu Vijay <himanshuvijay0804@gmail.com>
2026-06-22 20:32:48 +0530

    update

--- create_db.py
+++ create_db.py
@@ -1,41 +1,24 @@
 from database import get_connection
 
-
 conn = get_connection()
-
 cursor = conn.cursor()
 
-
 cursor.execute("""
-
 CREATE TABLE IF NOT EXISTS candidates (
-
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-
-    name TEXT,
-
-    email TEXT,
-
-    phone TEXT,
-
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(20),
     skills TEXT,
-
     resume_path TEXT,
-
-    status TEXT,
-
-    match_score REAL,
-
-    extracted_skills TEXT
-
+    status VARCHAR(50),
+    match_score FLOAT,
+    extracted_skills TEXT,
+    job_title VARCHAR(255)
 )
-
 """)
 
-
 conn.commit()
-
 conn.close()
-
 
 print("Database Created Successfully")
